423ba0b6e8d604988939956c69c9b444c69c5ed9
Karan Mahato <44karanmahato@gmail.com>
2023-04-29 08:44:44 +0545

    models, db and migration

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,29 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 # def home(request):
   # return HttpResponse('<h2>Blog homepage</h2>')
 
 
-posts = [
-  {
-    'author': 'Karan Mahato',
-    'title': 'Best Place To Learn Python From',
-    'content': 'Corey Schafer is the best Python teacher out there.',
-    'date_posted': 'April 24, 2023'
-  },
-  {
-    'author': 'Joe Nuts',
-    'title': 'Why the HTML/CSS course by SuperSimpleDev is the BEST!',
-    'content': 'You will actually build a thing not just mess around with the HTML syntax.',
-    'date_posted': 'April 25, 2023'
-  }
-]
-
 
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
 
   return render(request, 'blog_app/home.html', context)
