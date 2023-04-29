e43e9a7fffb452f003c048e3fe7b197a1b72bb72
Karan Mahato <44karanmahato@gmail.com>
2023-04-29 10:48:24 +0545

    user registration, crispy forms bs5

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users_app import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', users_views.register, name='register'),
     path('', include('blog_app.urls')),
 ]
