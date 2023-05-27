b132cc41c16e67fff18dc2aabc688bce3efd1bf3
Karan Mahato <44karanmahato@gmail.com>
2023-05-27 23:19:59 +0545

    add user profile and image & ability to update profile

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from users_app import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users_app/logout.html'), name='logout'),
     path('', include('blog_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
